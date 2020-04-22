39e25ae42d253534b596d9535a9bcace097499c8
Patrick Thomson <patrickt@github.com>
2020-04-22 11:04:18 -0400

    more prose.

--- init.el
+++ init.el
@@ -1,8 +1,7 @@
 ;;; init.el --- Patrick Thomson's Emacs setup.  -*- lexical-binding: t; -*-
 ;;
 ;;; Commentary:
-;; This file loads Org-mode and then loads the rest of the Emacs initialization from Emacs Lisp
-;; embedded in the literate Org-mode file: readme.org
+;; This file loads use-package, org-mode, and compiles and executes readme.org
 ;;
 ;;; Code:
 
@@ -10,24 +9,15 @@
 
 (setq package-enable-at-startup nil)
 
-(defun org-toggle-comment () "Please go away." (interactive))
-
-(unless (assoc-default "melpa" package-archives)
-  (add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t))
-
-(unless (assoc-default "melpa-stable" package-archives)
-  (add-to-list 'package-archives '("melpa-stable" . "https://stable.melpa.org/packages/") t))
+(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
+(add-to-list 'package-archives '("melpa-stable" . "https://stable.melpa.org/packages/") t)
 
 (package-initialize)
 
-;; Bootstrap use-package
 (unless (package-installed-p 'use-package)
   (package-refresh-contents)
   (package-install 'use-package))
 
-(setq emacs-dir (file-name-directory (or (buffer-file-name) load-file-name)))
-
-;; load up Org-mode and Org-babel
 (require 'org-install)
 (require 'ob-tangle)
 
