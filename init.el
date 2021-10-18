827868c2f1ba206bc729059dff2baa3546174a21
Patrick Thomson <patrickt@github.com>
2021-10-18 16:16:29 -0400

    bump

--- init.el
+++ init.el
@@ -7,14 +7,10 @@
 
 (require 'package)
 
-(setq package-enable-at-startup nil)
-
 (add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
 (add-to-list 'package-archives '("melpa-stable" . "https://stable.melpa.org/packages/") t)
 (add-to-list 'package-archives '("ublt" . "https://elpa.ubolonton.org/packages/"))
 (add-to-list 'package-archives '("org" . "https://orgmode.org/elpa/") t)
-
-(package-initialize)
 
 (unless (package-installed-p 'use-package)
   (package-refresh-contents)
