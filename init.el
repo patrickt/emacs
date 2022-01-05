e463ad34e02ff5384cfc7a1a9055e8e45ad11736
Patrick Thomson <patrickt@github.com>
2022-01-05 16:52:06 -0500

    better

--- init.el
+++ init.el
@@ -5,6 +5,11 @@
 ;;
 ;;; Code:
 
+(when (window-system)
+  (tool-bar-mode -1)
+  (scroll-bar-mode -1)
+  (tooltip-mode -1))
+
 (require 'package)
 
 (add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
