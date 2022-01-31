ae824f688767358683089bdfe8a268ca0b0267c8
Patrick Thomson <patrickt@github.com>
2022-01-31 16:11:13 -0500

    more

--- init.el
+++ init.el
@@ -11,18 +11,19 @@
   (tooltip-mode -1))
 
 (require 'package)
-
 (add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
-(add-to-list 'package-archives '("ublt" . "https://elpa.ubolonton.org/packages/"))
+(add-to-list 'package-archives '("ublt" . "https://elpa.ubolonton.org/packages/") t)
 (add-to-list 'package-archives '("org" . "https://orgmode.org/elpa/") t)
 (setq package-native-compile t)
 (unless (package-installed-p 'use-package)
+  (message "refreshing contents")
   (package-refresh-contents)
   (package-install 'use-package))
 
-(eval-when-compile (require 'use-package))
-(require 'org-install)
-(require 'ob-tangle)
+(eval-when-compile
+  (require 'use-package)
+  (require 'org-install)
+  (require 'ob-tangle))
 
 (defun reload-config ()
   "Reload the literate config from ~/.config/emacs/readme.org."
